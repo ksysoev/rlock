8015d7faf867acffb62a5c030f62307868a19a4b
Кирилл Сысоев <k.sysoev@me.com>
2023-10-07 21:13:30 +0800

    Adds basic documentation

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# redis-lock
-GoLang library for managing distributed locks
+# rlock
+rlock is a Redis-based distributed locking library for Go. It provides a simple API for acquiring and releasing locks, and uses Redis to ensure that locks are properly distributed across multiple processes.
+
+## Installation
+
+To install rlock, use `go get`:
+
+```sh
+go get github.com/redis/rlock
+```
+
+## Usage
+
+Here's an example of how to use rlock to acquire and release a lock:
+
+```golang
+redisClient := redis.NewClient(getRedisOptions())
+l := rlock.NewLocker(context.Background(), redisClient)
+
+lock, err := l.TryAcquire("my-lock", 1*time.Second)
+if err != nil {
+    // handle error
+}
+
+// do some work while holding the lock
+
+err = lock.Release()
+if err != nil {
+    // handle error
+}
+```
+
+## Contributing
+
+Contributions to rlock are welcome!
+
+
+## License
+
+rlock is licensed under the MIT License
--- rlock.go
+++ rlock.go
@@ -1,3 +1,4 @@
+// Package rlock provides a distributed locking mechanism using Redis.
 package rlock
 
 import (
@@ -8,29 +9,39 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+// ReleaseSignal is a type alias for a string that represents a signal to release a lock.
 type ReleaseSignal string
 
+// RedisStore is an interface that defines the Redis commands used by the Locker.
 type RedisStore interface {
+	// SetNX sets the value of a key, only if the key does not exist.
+	// It returns true if the key was set, false otherwise.
 	SetNX(ctx context.Context, key string, value any, ttl time.Duration) *redis.BoolCmd
+	// Del deletes one or more keys.
+	// It returns the number of keys that were deleted.
 	Del(ctx context.Context, keys ...string) *redis.IntCmd
 }
 
+// Locker is a struct that represents a distributed lock manager.
 type Locker struct {
 	storage   RedisStore
 	nameSpace string
 	ctx       context.Context
 }
 
+// Lock is a struct that represents a distributed lock.
 type Lock struct {
 	isLocked bool
 	key      string
 	locker   *Locker
 }
 
+// NewLocker creates a new Locker instance.
 func NewLocker(ctx context.Context, storage RedisStore) *Locker {
 	return &Locker{ctx: ctx, storage: storage}
 }
 
+// TryAcquire tries to acquire a lock for the given key with the specified time-to-live (TTL).
 func (l *Locker) TryAcquire(key string, ttl time.Duration) (*Lock, error) {
 	success, err := l.storage.SetNX(l.ctx, l.nameSpace+key, "1", ttl).Result()
 
@@ -45,15 +56,19 @@ func (l *Locker) TryAcquire(key string, ttl time.Duration) (*Lock, error) {
 	return newLock(key, l), nil
 }
 
+// release releases the lock for the given key.
 func (l *Locker) release(key string) error {
 	_, err := l.storage.Del(l.ctx, l.nameSpace+key).Result()
 	return err
 }
 
+// newLock creates a new Lock instance.
 func newLock(key string, locker *Locker) *Lock {
 	return &Lock{isLocked: true, key: key, locker: locker}
 }
 
+// Release releases the lock.
+// If the lock is already released, it returns an error.
 func (l *Lock) Release() error {
 	if l.isLocked {
 		err := l.locker.release(l.key)
@@ -66,9 +81,10 @@ func (l *Lock) Release() error {
 		return nil
 	}
 
-	return fmt.Errorf("lock is alread released")
+	return fmt.Errorf("lock is already released")
 }
 
+// IsLocked returns true if the lock is still held, false otherwise.
 func (l *Lock) IsLocked() bool {
 	return l.isLocked
 }
